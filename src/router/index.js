cca682c3b85561ba5a8a1d47f08cd226e194fb7c
zhaowk <ckt5532@dingtalk.com>
2022-02-17 10:26:27 +0800

    feat: url参数改为token, 省去获取步骤

--- src/router/index.js
+++ src/router/index.js
@@ -40,20 +40,24 @@ router.beforeEach(async (to, from, next) => {
   }
   if (!whiteList.includes(to.path)) {
     // 判断是否有携带工号参数, 没有则判断token是否存在
-    if (to.query.oaNum) {
-      // 根据工号获取token
-      const res = await Vue.prototype.$api.oa.getToken({ oaNum: to.query.oaNum })
-      if (res && res.code === 200) {
-        window.localStorage.setItem('token', res.data)
-        return next({ path: '/list' })
-      } else {
-        console.log(res.msg || 'token获取失败')
-        return router.replace('/401')
-      }
+    // if (to.query.oaNum) {
+    //   // 根据工号获取token
+    //   const res = await Vue.prototype.$api.oa.getToken({ oaNum: to.query.oaNum })
+    //   if (res && res.code === 200) {
+    //     window.localStorage.setItem('token', res.data)
+    //     return next({ path: '/list' })
+    //   } else {
+    //     console.log(res.msg || 'token获取失败')
+    //     return router.replace('/401')
+    //   }
+    // } else {
+    if (to.query.token) {
+      window.localStorage.setItem('token', to.query.token)
+      return next({ path: '/list' })
     } else {
       const token = window.localStorage.getItem('token')
       if (!token) {
-        router.replace('/403')
+        router.replace('/401')
         return next()
       }
     }
